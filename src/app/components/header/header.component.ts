c848e13719e20317382674d38bdf075fb53c0ead
Vinita Rajoria <imvinitarajoria@gmail.com>
2021-05-21 23:16:58 +0530

    My-Tracker commit3

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import {UiService} from '../../services/ui.service';
+import {Subscription} from 'rxjs';
+import{Router} from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Tracker-demo';
+  title: string = 'My-Tracker';
+  showAddTask:boolean= true;
+  subscription!: Subscription;
 
-
-  constructor() { }
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription= this.uiService
+    .onToggle()
+    .subscribe((value) => (this.showAddTask  = value));
+  }
 
   ngOnInit(): void {}
 
   toggleAddTask(){
-    console.log('toggle');
-  }
+    this.uiService.toggleAddTask();
+    }
 
+      hasRoute(route:string){
+        return this.router.url ===route;
+      }
 }
